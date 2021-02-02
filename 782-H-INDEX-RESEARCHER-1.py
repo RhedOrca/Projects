eecfa770cf870019707a940357cf5bdaba382d14
Tracing Vines <thenameisorca@gmail.com>
2021-02-02 07:40:30 -0800

    782-Find a researcher's H-Index

--- 782-H-INDEX-RESEARCHER-1.py
+++ 782-H-INDEX-RESEARCHER-1.py
@@ -1,5 +1,16 @@
-references = [4, 3, 0, 1, 5]
+from random import randint
 
-maxrefs = 0
+papers = []
+for i in range(0, randint(3, 20)):
+    papers.append(randint(0, 20))
+papers.sort()
+papers.reverse()
+print(str(papers))
+#  Everything above generates the test citation list
 
-for paper in references:
+for i in range(0, len(papers)):
+    if papers[i] <= i:
+        input("H-index is " + str(i))
+        break
+
+
